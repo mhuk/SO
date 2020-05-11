7f40cbe38d1d10707a6685e5749cc4e400339e87
mhuk <Huk.Magdalena@wp.pl>
2020-05-11 18:28:44 +0200

    Add scripts and lab. report

---
+++ lab5zad1.ps1
@@ -0,0 +1,30 @@
+﻿#zadanie1
+
+$dir_name = Read-Host 'Get directory name'
+$number = Read-Host 'Get number'
+$Currentlocation = Get-Location #aktualna lokalizacja pliku
+
+if ([string]::IsNullOrWhiteSpace($dir_name) -OR [string]::IsNullOrWhiteSpace($number))
+{
+    "This variable is empty"
+    exit 1
+}
+
+while (Test-Path -Path "$Currentlocation\$dir_name")
+{
+    if ([string]::IsNullOrWhiteSpace($dir_name))
+    {
+        "Directory name is empty"
+        exit 1
+    }
+    Write-Output "Directory Exists Already"
+    $dir_name = Read-Host 'Get directory name'
+}
+#utworz folder
+New-Item -Path $Currentlocation\$dir_name -ItemType directory
+
+For ($i=1; $i -le $number; $i++)
+{
+    #utworz plik tekstowy
+    New-Item -Path $Currentlocation\$dir_name\file$i.txt -ItemType File 
+}
---
+++ lab5zad2.ps1
@@ -0,0 +1,19 @@
+﻿#zadanie2
+write-host "File name [.txt]:" $args[0]
+write-host "Char:" $args[1]
+$file = $args[0]
+$char = $args[1]
+
+if ([string]::IsNullOrWhiteSpace($file) -OR [string]::IsNullOrWhiteSpace($char))
+{
+    "This variable is empty"
+    exit 1
+}
+
+$Currentlocation = Get-Location #current location of script
+$txt = Get-Content $Currentlocation'\'$file'.txt' #location file.txt
+
+$total = ($txt | Select-String -Pattern [$char] -AllMatches).Matches.Count #number found chars
+write-host "Count:" $total "matches"
+
+
